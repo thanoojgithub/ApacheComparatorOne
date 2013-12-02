ab7c6df2727f228917370873682c1e8fc40309ae
thanoojgithub <javaeecub@gmail.com>
2013-12-03 01:35:13 +0530

    Apache Comparator

---
+++ src/main/java/com/apachecomparatorone/Person.java
@@ -0,0 +1,41 @@
+package com.apachecomparatorone;
+
+public class Person {
+	
+	private int id;
+	private String name;
+	
+	public Person() {}
+	
+	
+
+	public Person(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return id +" "+name;
+	}
+
+}
